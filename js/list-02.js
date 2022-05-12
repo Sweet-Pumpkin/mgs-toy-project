0ddf444e737d3d0d6e8b35930f2c50fbf5b2d93e
Sweet-Pumpkin <2013ssh3y95@naver.com>
2022-05-12 14:29:40 +0900

    feat: list-02.js 코드 수정

--- js/list-02.js
+++ js/list-02.js
@@ -3,65 +3,73 @@ fetch("./json/list-02.json")
   return res.json()
 })
 .then((obj) => {
-  List2(obj, "2022.05.06", "오늘");
-  List2(obj, "2022.05.05", "어제");
-  List2(obj, "2022.05.04", "2일 전");
+  List2(obj);
 })
-  
-  function List2(obj, D1, D2) {
-    const ulEls = document.querySelectorAll('#subReceipt');
-    ulEls.forEach(ulEl => {
-      
-      const dataWrap = document.createElement('div');
-      const dateName = document.createElement('span');
-      dateName.className = "dateName";
-      dateName.textContent = D2;
-  
-      const plusPrice = document.createElement('span');
-      plusPrice.className ="plusPrice";
-  
-      const D1Price = obj.map(
-        v => v.date === D1 && v.inOut === 'out' ? v.price : null)
-        .reduce((a, b) => {return a + b}, 0
-      );
-      
-      const changeD1Price = D1Price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-  
-      plusPrice.textContent = `${changeD1Price}원 지출`;
-      
-      ulEl.appendChild(dataWrap);
-      dataWrap.appendChild(dateName);
-      dataWrap.appendChild(plusPrice);
 
-      for (let i = 0; i < obj.length; i++) {
-        const date = obj[i].date;
-        const inOut = obj[i].inOut;
-        const item = obj[i].item;
-        const price = obj[i].price;
-        const changePrice = price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  function List2(obj) {
+
+    // 날짜 배열 추출
+    const date = obj.map(v => v.date);
+    const arr = new Array(date);
+    const arrZero = arr[0];
+    const arrUni = arrZero.filter((val, idx) => {
+      return arrZero.indexOf(val) === idx;
+    })
+    const arrSorts = arrUni.reverse();
+    for (let i = 0; i < arrSorts.length; i++) {
+      const D = arrSorts[i]; // 날짜 추출
+
+      // 지출 합산 & 날짜 표시 추출
+      const ulEls = document.querySelectorAll('#subReceipt');
+      ulEls.forEach(ulEl => {
+        const dataWrap = document.createElement('div');
+        const dateName = document.createElement('span');
+        dateName.className = "dateName";
+        dateName.textContent = D;
+    
+        const plusPrice = document.createElement('span');
+        plusPrice.className ="plusPrice";
+    
+        const DPrice = obj.map(
+          v => v.date === D && v.inOut === 'out' ? v.price : null)
+          .reduce((a, b) => {return a + b}, 0
+        );
+        
+        const changeDPrice = DPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    
+        plusPrice.textContent = `${changeDPrice}원 지출`;
+        
+        ulEl.appendChild(dataWrap);
+        dataWrap.appendChild(dateName);
+        dataWrap.appendChild(plusPrice);
   
-        if (date === D1) {
-          const liItem = document.createElement('div');
-          const liPrice = document.createElement('div');
-          const listWrap = document.createElement('li');
-  
-          liItem.textContent = `${item}`;
-  
-          if (inOut === 'out') {
-            liPrice.classList.add('out');
-            
-            liPrice.textContent = `${changePrice}원`; 
-          } else {
-            liPrice.classList.add('in');
-            liPrice.textContent = `${changePrice}원`; 
+        for (let i = 0; i < obj.length; i++) {
+          const date = obj[i].date;
+          const inOut = obj[i].inOut;
+          const item = obj[i].item;
+          const price = obj[i].price;
+          const changePrice = price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    
+          if (date === D) {
+            const liItem = document.createElement('div');
+            const liPrice = document.createElement('div');
+            const listWrap = document.createElement('li');
+    
+            liItem.textContent = `${item}`;
+    
+            if (inOut === 'out') {
+              liPrice.classList.add('out');
+              
+              liPrice.textContent = `${changePrice}원`; 
+            } else {
+              liPrice.classList.add('in');
+              liPrice.textContent = `${changePrice}원`; 
+            }
+            ulEl.appendChild(listWrap)
+            listWrap.appendChild(liItem);
+            listWrap.appendChild(liPrice);
           }
-          ulEl.appendChild(listWrap)
-          listWrap.appendChild(liItem);
-          listWrap.appendChild(liPrice);
         }
-      }
-    });
+      })
+    }
   }
-
-
-  
